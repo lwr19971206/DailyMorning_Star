adc8136d616b74b0709e3d205126974f1568f36a
Winter Lin <56198892+lwr19971206@users.noreply.github.com>
2022-09-25 18:03:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -141,6 +141,10 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
                 "value": "{} {}".format(today, week),
                 "color": get_color()
             },
+            "lindate": {
+                "value": "{} {}".format(today, week),
+                "color": get_color()
+            },
             "city": {
                 "value": city_name,
                 "color": get_color()
@@ -180,6 +184,8 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
             birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
+        
+        
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
